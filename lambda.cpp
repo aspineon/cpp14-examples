d8b075b5f0949fbac1d2fd9573a7904a36614275
Shao-Chuan Wang <shaochuan.wang@gmail.com>
2014-12-23 22:52:53 -0800

    Add generic lambda function example.

--- lambda.cpp
+++ lambda.cpp
@@ -6,9 +6,25 @@ using voidfunc = void (*)();
 
 /**
  * Testing the feature of a basic lambda function.
+ * Every instance of a lambda defines a new type.
+ * For non-capturing lambda, it can be casted into a
+ * function pointer implicitly.
  */
 TEST(LAMBDA_TEST, func_type) {
   voidfunc empty_lambda_func = [](){}; // implicit casting here.
-  bool is_same = std::is_same<voidfunc, decltype(empty_lambda_func)>::value;
+  auto lambda_func = [](){};
+  bool is_same = std::is_same<voidfunc, decltype(lambda_func)>::value;
+  EXPECT_FALSE(is_same);
+  is_same = std::is_same<voidfunc, decltype(empty_lambda_func)>::value;
   EXPECT_TRUE(is_same);
+}
+
+/**
+ * Testing the feature of generic lambda function.
+ * Look at the argument, it's an 'auto'.
+ */
+TEST(LAMBDA_TEST, generic_lambda) {
+  auto getsize = []( auto const& c ) { return c.size(); };
+  std::vector<int> v {1,2,3};
+  EXPECT_EQ(3, getsize(v));
 }
